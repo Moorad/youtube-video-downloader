7207d5ccb5a55831a43451eb1b7764991de90f63
balajinikhil <balajiedward007@gmail.com>
2020-04-13 20:38:40 +0530

    fix-2

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-let Btn = document.getElementById('mp4');
-let URLinput = document.querySelector('.URL-input');
-let select = document.querySelector('.opt');
-let serverURL = 'http://localhost:4000';
+let Btn = document.getElementById("btn");
+let URLinput = document.querySelector(".URL-input");
+let select = document.querySelector(".opt");
+let serverURL = "http://localhost:4000";
 
-Btn.addEventListener('click', () => {
-    if (!URLinput.value) {
-        alert('Enter YouTube URL');
-    } else {
-        if (select.value == 'mp3') {
-            redirectMp3(URLinput.value);
-        } else if (select.value == 'mp4') {
-            redirectMp4(URLinput.value);
-        }
+Btn.addEventListener("click", () => {
+  if (!URLinput.value) {
+    alert("Enter YouTube URL");
+  } else {
+    if (select.value == "mp3") {
+      redirectMp3(URLinput.value);
+    } else if (select.value == "mp4") {
+      redirectMp4(URLinput.value);
     }
+  }
 });
 
 function redirectMp3(query) {
-    window.location.href = `${serverURL}/downloadmp3?url=${query}`;
+  window.location.href = `${serverURL}/downloadmp3?url=${query}`;
 }
 
 function redirectMp4(query) {
-    window.location.href = `${serverURL}/downloadmp4?url=${query}`;
+  window.location.href = `${serverURL}/downloadmp4?url=${query}`;
 }
