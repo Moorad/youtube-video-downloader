e8ede17de7637753a928bd2a8f22a2c7236c4084
balajinikhil <balajiedward007@gmail.com>
2020-04-13 20:37:11 +0530

    fix-2

--- Server/index.js
+++ Server/index.js
@@ -1,49 +1,47 @@
-const express = require('express');
-const cors = require('cors');
-const ytdl = require('ytdl-core');
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
 
 const app = express();
 
 app.use(cors());
 
 app.listen(4000, () => {
-    console.log('Server Works !!! At port 4000');
+  console.log("Server Works !!! At port 4000");
 });
 
-app.get('/downloadmp3', async (req, res, next) => {
-    try {
-        var url = req.query.url;
-        let title = 'audio';
-        await ytdl.getBasicInfo(url, { format: 'mp4' }, (err, info) => {
-            title = info.player_response.videoDetails.title;
-        });
+app.get("/downloadmp3", async (req, res, next) => {
+  try {
+    var url = req.query.url;
+    let title = "audio";
+    await ytdl.getBasicInfo(url, { format: "mp4" }, (err, info) => {
+      title = info.player_response.videoDetails.title;
+    });
 
-        res.header('Content-Disposition', `attachment; filename='${title}.mp3'`);
-        ytdl(url, {
-            format: 'mp3',
-            filter: 'audioonly',
-        }).pipe(res);
-    } catch (err) {
-        console.error(err);
-    }
+    res.header("Content-Disposition", `attachment; filename="${title}.mp3"`);
+    ytdl(url, {
+      format: "mp3",
+      filter: "audioonly",
+    }).pipe(res);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-app.get('/downloadmp4', async (req, res, next) => {
-    try {
-        let URL = req.query.url;
-        let title = 'video';
+app.get("/downloadmp4", async (req, res, next) => {
+  try {
+    let URL = req.query.url;
+    let title = "video";
 
-        await ytdl.getBasicInfo(URL, { format: 'mp4' }, (err, info) => {
-            title = info.player_response.videoDetails.title;
-        });
+    await ytdl.getBasicInfo(URL, { format: "mp4" }, (err, info) => {
+      title = info.player_response.videoDetails.title;
+    });
 
-        res.header('Content-Disposition', `attachment; filename='${title}.mp4'`);
-        ytdl(URL, {
-            format: 'mp4',
-        }).pipe(res);
-    } catch (err) {
-        console.error(err);
-    }
+    res.header("Content-Disposition", `attachment; filename="${title}.mp4"`);
+    ytdl(URL, {
+      format: "mp4",
+    }).pipe(res);
+  } catch (err) {
+    console.error(err);
+  }
 });
-
-
